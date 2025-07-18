3037af079ca26b80f10e600a3b53f1bf203107f4
liaohuanxuan.zzq <liaohuanxuan.zzq@bytedance.com>
2025-07-18 22:14:48 +0800

    install
    Signed-off-by: liaohuanxuan.zzq <liaohuanxuan.zzq@bytedance.com>

--- preinstall.sh
+++ preinstall.sh
@@ -1,3 +1,5 @@
+pip install torch==2.7.0 torchvision==0.22.0 torchaudio==2.7.0 --index-url https://download.pytorch.org/whl/cu126
+pip install flash_attn
 pip install -U transformers
 pip uninstall tensorflow -y
 pip install kvpress --quiet
