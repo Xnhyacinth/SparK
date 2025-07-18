3501a62537895729a311dac83c6521de895a94aa
liaohuanxuan.zzq <liaohuanxuan.zzq@bytedance.com>
2025-07-18 22:10:07 +0800

    metric
    Signed-off-by: liaohuanxuan.zzq <liaohuanxuan.zzq@bytedance.com>

--- down.py
+++ down.py
@@ -2,19 +2,6 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
 
-tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-3.1-8B-Instruct", token="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")
-model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-3.1-8B-Instruct", token="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")
-
-# tokenizer = AutoTokenizer.from_pretrained("meta-llama/Meta-Llama-3-8B-Instruct", token="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")
-# model = AutoModelForCausalLM.from_pretrained("meta-llama/Meta-Llama-3-8B-Instruct", token="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") yao
-
-# mistralai/Mistral-Small-3.1-24B-Instruct-2503  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
-
-# Load model directly
-# from transformers import AutoTokenizer, AutoModelForCausalLM
-
-# tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.3", token="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")
-# model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-Instruct-v0.3", token="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") xn
 
 # "simonjegou/ruler"
 from datasets import load_dataset
