3cb4115463f0e53344c685d168f07cc7b69cfb58
liaohuanxuan.zzq <liaohuanxuan.zzq@bytedance.com>
2025-07-19 11:27:21 +0800

    install
    Signed-off-by: liaohuanxuan.zzq <liaohuanxuan.zzq@bytedance.com>

--- down.py
+++ down.py
@@ -24,27 +24,27 @@ from datasets import load_dataset
 # ]:
 #     ds = load_dataset("MaxJeblick/InfiniteBench", task)
 
-# for task in [
-#     "narrativeqa",
-#     "qasper",
-#     "multifieldqa_en",
-#     "multifieldqa_zh",
-#     "hotpotqa",
-#     "2wikimqa",
-#     "musique",
-#     "dureader",
-#     "gov_report",
-#     "qmsum",
-#     "multi_news",
-#     "vcsum",
-#     "trec",
-#     "triviaqa",
-#     "samsum",
-#     "lsht",
-#     "passage_count",
-#     "passage_retrieval_en",
-#     "passage_retrieval_zh",
-#     "lcc",
-#     "repobench-p"
-# ]: 
-#     ds = load_dataset("Xnhyacinth/LongBench", task)
+for task in [
+    "narrativeqa",
+    "qasper",
+    "multifieldqa_en",
+    "multifieldqa_zh",
+    "hotpotqa",
+    "2wikimqa",
+    "musique",
+    "dureader",
+    "gov_report",
+    "qmsum",
+    "multi_news",
+    "vcsum",
+    "trec",
+    "triviaqa",
+    "samsum",
+    "lsht",
+    "passage_count",
+    "passage_retrieval_en",
+    "passage_retrieval_zh",
+    "lcc",
+    "repobench-p"
+]: 
+    ds = load_dataset("Xnhyacinth/LongBench", task)
--- preinstall.sh
+++ preinstall.sh
@@ -1,8 +1,13 @@
 pip install torch==2.7.0 torchvision==0.22.0 torchaudio==2.7.0 --index-url https://download.pytorch.org/whl/cu126
-pip install flash_attn
+pip install flash_attn==2.7.4.post1
+pip install fire
+pip install nltk
+pip install bert_score
+pip install rouge
 pip install -U transformers
 pip uninstall tensorflow -y
 pip install kvpress --quiet
 pip install scikit-learn==1.5.1
+pip install datasets==2.21.0
 pip install jieba
 pip install fuzzywuzzy
